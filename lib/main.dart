d6bd2611f742a0059596d56af989211300edf031
TerrenceBeckham <terrencebeckham@icloud.com>
2022-03-04 09:30:56 +0200

    Added and centered dice image widgets

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,21 @@ void main() {
   );
 }
 
-//This is a test of git
 // ignore: use_key_in_widget_constructors
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Image.asset('images/dice1.png'),
+          ),
+          Expanded(
+            child: Image.asset('images/dice1.png'),
+          ),
+        ],
+      ),
+    );
   }
 }
