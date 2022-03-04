d84737c0736ca2be4b7789fa38934c161a2a0988
TerrenceBeckham <terrencebeckham@icloud.com>
2022-03-04 19:54:43 +0200

    Changed DicePage into a stateful widget and implemented button logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,22 +17,46 @@ void main() {
 }
 
 // ignore: use_key_in_widget_constructors
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceButton = 1;
+  var rightDiceButton = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: [
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  if (leftDiceButton != 6) {
+                    leftDiceButton++;
+                  } else {
+                    leftDiceButton = 1;
+                  }
+                });
+              },
+              child: Image.asset('images/dice$leftDiceButton.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  if (rightDiceButton != 6) {
+                    rightDiceButton++;
+                  } else {
+                    rightDiceButton = 1;
+                  }
+                });
+              },
+              child: Image.asset('images/dice$rightDiceButton.png'),
             ),
           ),
         ],
